2a0e311449d8f594bacd8331864ea06d72c83385
Thomas Watson Steen <w@tson.dk>
2015-07-09 22:56:23 +0200

    Use the 'standard' linter

--- example.js
+++ example.js
@@ -4,10 +4,10 @@ var ghsign = require('./')
 var sign = ghsign.signer(username)
 var verify = ghsign.verifier(username)
 
-sign('hello', function(err, sig) {
+sign('hello', function (err, sig) {
   if (err) throw err
-  verify('hello', sig, function(err, valid) {
+  verify('hello', sig, function (err, valid) {
     if (err) throw err
-    console.log('hello was signed by '+username+'? '+valid)
+    console.log('hello was signed by ' + username + '? ' + valid)
   })
 })
